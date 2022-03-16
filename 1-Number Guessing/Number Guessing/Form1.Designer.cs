0f76acbca24b17dd456182922d5a00d7969fb701
Halil Can Yılmaz <44755095+ylmzhcy@users.noreply.github.com>
2022-03-16 18:32:30 +0300

    Project Complete
    v1

---
+++ 1-Number Guessing/Number Guessing/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Number_Guessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEntry = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnTry = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSecond);
+            this.groupBox1.Controls.Add(this.txtFirst);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnTry);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtEntry);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 140);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Find Hiding Number";
+            // 
+            // txtEntry
+            // 
+            this.txtEntry.Location = new System.Drawing.Point(33, 98);
+            this.txtEntry.Name = "txtEntry";
+            this.txtEntry.Size = new System.Drawing.Size(50, 20);
+            this.txtEntry.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(33, 27);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 34);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTry
+            // 
+            this.btnTry.Location = new System.Drawing.Point(89, 96);
+            this.btnTry.Name = "btnTry";
+            this.btnTry.Size = new System.Drawing.Size(75, 23);
+            this.btnTry.TabIndex = 3;
+            this.btnTry.Text = "Try";
+            this.btnTry.UseVisualStyleBackColor = true;
+            this.btnTry.Click += new System.EventHandler(this.btnTry_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "???";
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(249, 35);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(55, 20);
+            this.txtFirst.TabIndex = 5;
+            this.txtFirst.Text = "0";
+            this.txtFirst.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(310, 35);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(55, 20);
+            this.txtSecond.TabIndex = 6;
+            this.txtSecond.Text = "100";
+            this.txtSecond.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Between";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(186, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 143);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Guessing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTry;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtEntry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.TextBox txtFirst;
+    }
+}
+
---
+++ 1-Number Guessing/Number Guessing/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Guessing
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int randomNum;
+        int entryNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateRandomNum(int firstNum,int secondNum)
+        {
+            randomNum = rnd.Next(firstNum, secondNum);
+            //label2.Text = randomNum.ToString();
+            label2.Text = "???";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtFirst.Text != "" && txtSecond.Text != "")
+            {
+                CreateRandomNum(Convert.ToInt32(txtFirst.Text), Convert.ToInt32(txtSecond.Text));
+                btnStart.Enabled = false;
+                txtFirst.Enabled = false;
+                txtSecond.Enabled = false;
+                btnTry.Enabled = true;
+                label1.Text = "";
+                txtEntry.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Numbers");
+            }
+        }
+
+        private void btnTry_Click(object sender, EventArgs e)
+        {
+            if (txtEntry.Text != "")
+            {
+                entryNum = Convert.ToInt32(txtEntry.Text);
+
+                if (entryNum <= randomNum)
+                {
+                    label1.Text = "Entry Num is Too Low!";
+                }
+                else if (entryNum >= randomNum)
+                {
+                    label1.Text = "Entry Num is Too High!";
+                }
+                
+                if (entryNum == randomNum)
+                {
+                    label1.Text = "Correct !! You Win!";
+                    label2.Text = randomNum.ToString();
+                    btnStart.Enabled = true;
+                    txtFirst.Enabled = true;
+                    txtSecond.Enabled = true;
+                    txtEntry.Text = "";
+                    txtEntry.Enabled = false;
+                    btnTry.Enabled = false;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter Number");
+            }
+        }
+    }
+}
